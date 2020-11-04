7d4a280450b8accac81ef0599c635fb1bc348ca4
delanohehakaja <d.d.hehakaja@gmail.com>
2020-11-05 00:37:05 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
 // JavaScript Document
 
+// maakt de variabel deButton aan om "nav button" aan te kunnen roepen //
 var deButton = document.querySelector("nav button");
 
+// Zorgt ervoor dat de animatie wordt geactiveerd dmv klikken // 
 deButton.addEventListener("click", toggleMenu);
 
+//Na de klik wordt toggleMenu aangeroepen//
+
+//hier geef je toggleMenu de functie om toonmenu aan te tonen//
 function toggleMenu(event) {
   deNav = event.target.parentNode;
   deNav.classList.toggle("toonMenu");
